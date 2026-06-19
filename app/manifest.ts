4898b2011e9283837b5955782b717ea878268e22
devilbatman <icharichard@gmail.com>
2026-06-19 21:42:32 +0700

    fix: correct PWA manifest purpose type

--- app/manifest.ts
+++ app/manifest.ts
@@ -21,7 +21,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/monogram-pd-192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/apple-touch-icon.png',
